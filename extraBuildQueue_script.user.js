1f031b8d7844a809cae78b43db26b4e2814b3a1a
nykocz <nykadamec@gmail.com>
2024-11-13 17:10:07 +0100

    Update extraBuildQueue_script.user.js

--- extraBuildQueue_script.user.js
+++ extraBuildQueue_script.user.js
@@ -67,7 +67,7 @@ function injectExtraBuildQueue(availableBuildingsImgs, availableBuildingLevels, 
             
         }
 
-        upgradeLink
+
 
         var upgradeCell = document.createElement('td');
         upgradeCell.appendChild(upgradeLink);
